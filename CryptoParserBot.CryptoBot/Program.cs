e7b9f459f9fc62166d67215f5cc9fc774482d58a
Stanislav Forester <baxtoban555308775@outlook.com>
2022-07-08 14:49:09 +0300

    last upd

--- CryptoParserBot.CryptoBot/Program.cs
+++ CryptoParserBot.CryptoBot/Program.cs
@@ -1,6 +1,28 @@
 ﻿using CryptoParserBot.ExchangeClient_s.Clients;
 
 var client = new NiceHashClient();
-var balances = client.GetAccountBalance();
-balances.ForEach(x =>
-    Console.WriteLine($"Cur: {x.Currency}\nAvail bal: {x.AvailableBalance}"));
+
+
+// пока просто хардкодим валюту и максимальную цену
+var currency = "BTCUSDT";
+var setPrice = 25000;
+
+// примерная логика бота (просто пример)
+while (true)
+{
+    Thread.Sleep(5000);
+
+    // получаем текущую цену BTCUSDT
+    var totalPrice = client.GetCurrencyPrice(currency);
+
+    // если текущая цена меньше установленной, то ничего не делаем
+    if (totalPrice <= setPrice) continue;
+
+    // если цена больше установленной
+    // получаем баланс BTC на аккаунте
+    var balances =
+        client.GetAccountBalance().FirstOrDefault(x => x.Currency == "BTC");
+
+    // создаем ордер на продажу всех BTC на аккаунте
+    var order = client.CreateSellOrder(currency, balances.AvailableBalance, totalPrice);
+}
