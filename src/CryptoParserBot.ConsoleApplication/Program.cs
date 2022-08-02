fee2317e2f30e29ef2f4ab223fb169e01e5d8248
Stanislav Forester <baxtoban555308775@outlook.com>
2022-08-02 17:22:08 +0300

    add console logic

--- src/CryptoParserBot.ConsoleApplication/Program.cs
+++ src/CryptoParserBot.ConsoleApplication/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using CryptoParserBot.CryptoBot;
+using CryptoParserBot.CryptoBot.Models.Configs;
+using CryptoParserBot.ExchangeClients.Clients;
+using static System.String;
 
-Console.WriteLine("Hello, World!");
+var cfg = ConfigInitializer.GetClientConfig();
+
+var client = new NiceHashClient(
+    key: cfg.Key, // public key
+    secretKey: cfg.SecretKey, // secret key
+    organizationId: cfg.OrgID // organization id
+);
+
+Console.Write("Какой коин продаем: ");
+var sellCoin = Console.ReadLine()?.ToUpper();
+
+Console.Write("Какой коин покупаем: ");
+var buyCoin = Console.ReadLine()?.ToUpper();
+
+Console.Write($"Лимит цена({buyCoin}): ");
+var currencyRes = decimal.TryParse(
+    Console.ReadLine()?.Replace('.', ','), out var currencyLimit);
+
+Console.Write($"Минимальный баланс({sellCoin}): ");
+var balanceRes = decimal.TryParse(
+    Console.ReadLine()?.Replace('.', ','), out var balanceLimit);
+
+if( currencyRes is false ||
+    balanceRes is false || 
+    IsNullOrEmpty(sellCoin) ||
+    IsNullOrEmpty(buyCoin))
+{
+    Console.WriteLine("Ошибка ввода данных!");
+    return;
+}
+
+var bot = new CryptoBot(client, new CurrencyInfo
+{
+    FirstCoin = sellCoin,
+    SecondCoin = buyCoin,
+    PriceLimit = currencyLimit,
+    BalanceLimit = balanceLimit
+});
+bot.StartBot();
