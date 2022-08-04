8f35d7812599ff97acd02b531f8f5d6864dc349c
Stanislav Forester <baxtoban555308775@outlook.com>
2022-08-04 12:26:59 +0300

    add f

---
+++ src/CryptoParserBot.AdditionalToolLibrary/PathHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace CryptoParserBot.AdditionalToolLibrary;
+
+public static class PathHelper
+{
+    public static string ProjectPath =>
+        Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\.."));
+    /// <summary>
+    /// Create folders
+    /// </summary>
+    /// <param name="filePaths"></param>
+    public static void CheckForPathExists(params string[] filePaths)
+    {
+        foreach (var path in filePaths)
+        {
+            if (Directory.Exists(path) is true) continue;
+            
+            var file = Directory.CreateDirectory(path);
+        }
+    }
+}
